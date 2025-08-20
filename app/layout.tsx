c9fd8d9b1fe62f4b039a71b9ba8fd988937209a5
Atabkam <atabue@proton.me>
2025-08-20 15:17:02 -0600

    Fix Hydration Error

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,8 +24,9 @@ export default function RootLayout({
     <html
       lang="en"
       className={`bg-white dark:bg-gray-950 text-black dark:text-white ${manrope.className}`}
+      suppressHydrationWarning
     >
-      <body className="min-h-[100dvh] bg-gray-50">
+      <body className="min-h-[100dvh] bg-gray-50" suppressHydrationWarning>
         <SWRConfig
           value={{
             fallback: {
