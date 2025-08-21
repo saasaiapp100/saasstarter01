4b93affb7e30d0d82b76fd6f0084b7b134dc2cae
Atabkam <atabue@proton.me>
2025-08-20 18:29:49 -0600

    Fix Build Attempt N0 5

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    ppr: true,
-    clientSegmentCache: true,
-    nodeMiddleware: true
-  }
-};
+const nextConfig = {};
 
 export default nextConfig;
